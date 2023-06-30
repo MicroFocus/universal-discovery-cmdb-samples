16be8946e9295e0f3363e12c37a324dfc9392de7
wusuirong <sui-rong.wu@microfocus.com>
2023-06-30 17:52:27 +0800

    Update license to Open Text

--- src/test/java/com/microfocus/ucmdb/rest/sample/discovery/CreateAWSZoneTest.java
+++ src/test/java/com/microfocus/ucmdb/rest/sample/discovery/CreateAWSZoneTest.java
@@ -1,16 +1,19 @@
 /**
  * Copyright 2023 Open Text
- * The only warranties for products and services of Open Text and its affiliates and licensors (“Open Text”) are as may be set forth in the express warranty statements accompanying such products and services.
+ * The only warranties for products and services of Open Text and its
+ * affiliates and licensors ("Open Text") are as may be set forth in the
+ * express warranty statements accompanying such products and services.
  * Nothing herein should be construed as constituting an additional warranty.
- * Open Text shall not be liable for technical or editorial errors or omissions contained herein.
+ * Open Text shall not be liable for technical or editorial errors or
+ * omissions contained herein.
  * The information contained herein is subject to change without notice.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
