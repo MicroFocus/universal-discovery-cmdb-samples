5ad284ae637797cf995cd23b191a9975757d7a84
dingmeng <mding@microfocus.com>
2024-01-23 16:09:38 +0800

    Copy Zone

---
+++ src/test/java/com/microfocus/ucmdb/rest/sample/discovery/CopyZoneTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2020 - 2023 Open Text
+ * The only warranties for products and services of Open Text and its
+ * affiliates and licensors ("Open Text") are as may be set forth in the
+ * express warranty statements accompanying such products and services.
+ * Nothing herein should be construed as constituting an additional warranty.
+ * Open Text shall not be liable for technical or editorial errors or
+ * omissions contained herein.
+ * The information contained herein is subject to change without notice.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.microfocus.ucmdb.rest.sample.discovery;
+
+import org.junit.Test;
+
+public class CopyZoneTest {
+
+    @Test
+    public void testMain() throws Exception {
+        CopyZone.main(new String[]{"${UCMDB_IP_ADDRESS}", "${UCMDB_PORT}", "${UCMDB_USERNAME}", "${UCMDB_PASSWORD}"});
+    }
+
+} 
